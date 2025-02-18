e7f75934358da68fc8674923dbeaff5b84cf9f78
Nam Phuong <157980092+num153@users.noreply.github.com>
2025-02-18 13:46:04 +0700

    Add files via upload

---
+++ BTH1/StructTime/StructTime/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace StructTime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHour = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSec = new System.Windows.Forms.TextBox();
+            this.lbShow = new System.Windows.Forms.TextBox();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(132, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter hour: ";
+            // 
+            // txtHour
+            // 
+            this.txtHour.Location = new System.Drawing.Point(259, 75);
+            this.txtHour.Name = "txtHour";
+            this.txtHour.Size = new System.Drawing.Size(136, 22);
+            this.txtHour.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(132, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Enter Minute: ";
+            // 
+            // txtMin
+            // 
+            this.txtMin.Location = new System.Drawing.Point(259, 128);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(136, 22);
+            this.txtMin.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.Location = new System.Drawing.Point(132, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Enter Second: ";
+            // 
+            // txtSec
+            // 
+            this.txtSec.Location = new System.Drawing.Point(259, 191);
+            this.txtSec.Name = "txtSec";
+            this.txtSec.Size = new System.Drawing.Size(136, 22);
+            this.txtSec.TabIndex = 1;
+            // 
+            // lbShow
+            // 
+            this.lbShow.Location = new System.Drawing.Point(364, 250);
+            this.lbShow.Multiline = true;
+            this.lbShow.Name = "lbShow";
+            this.lbShow.Size = new System.Drawing.Size(249, 46);
+            this.lbShow.TabIndex = 2;
+            // 
+            // btnShow
+            // 
+            this.btnShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnShow.Location = new System.Drawing.Point(179, 250);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(110, 51);
+            this.btnShow.TabIndex = 3;
+            this.btnShow.Text = "Show Time";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.lbShow);
+            this.Controls.Add(this.txtSec);
+            this.Controls.Add(this.txtMin);
+            this.Controls.Add(this.txtHour);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Timer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHour;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSec;
+        private System.Windows.Forms.TextBox lbShow;
+        private System.Windows.Forms.Button btnShow;
+    }
+}
+
---
+++ BTH1/StructTime/StructTime/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StructTime
+{
+    public partial class Form1 : Form
+    {
+        struct Time
+        {
+            int gio, phut, giay;
+            public int Hour { 
+                get {  return gio; }
+                set { gio = value; } 
+            }
+            public int Min
+            {
+                get { return phut; }
+                set { phut = value; }
+            }
+            public int Sec
+            {
+                get { return giay; }
+                set { giay = value; }
+            }
+            public Time(int h, int m, int s)
+            {
+                gio = h;
+                phut = m;
+                giay = s;
+
+            }
+            public void ChuanHoa()
+            {
+                int t = giay / 60;
+                giay=giay % 60;
+                phut = phut + t;
+                t=phut / 60;
+                phut=phut % 60;
+                gio = gio + t;
+                gio = (gio >= 24 ? gio % 24 : gio);
+            }
+            public String showTime()
+            {
+                ChuanHoa();
+                return String.Format("{0} : {1:00} : {2:00}",gio,phut, giay);
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            Time time = new Time(int.Parse(txtHour.Text),
+                                    int.Parse(txtMin.Text),
+                                    int.Parse(txtSec.Text));
+            lbShow.Text = time.showTime();
+        }
+    }
+}
