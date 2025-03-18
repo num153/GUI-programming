b40f539a44fb497de66f5d075d6dabf0d78e5d47
Nam Phuong <157980092+num153@users.noreply.github.com>
2025-03-18 23:10:39 +0700

    su dung mqh cac class voi nhau

---
+++ BTH3/ClassPhanSo/ClassPhanSo/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassPhanSo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCong_Click(object sender, EventArgs e)
+        {
+            lbDau.Text = btnCong.Text;
+
+            try
+            {
+                PhanSo ps1 = new PhanSo(int.Parse(txtTuSo1.Text), int.Parse(txtMauSo1.Text));
+                PhanSo ps2 = new PhanSo(int.Parse(txtTuSo2.Text), int.Parse(txtMauSo2.Text));
+                PhanSo kq = ps1.Cong(ps2);
+                txtTuSo3.Text = kq.Tuso.ToString();
+                txtMauSo3.Text = kq.Mauso.ToString();
+            }
+            catch(FormatException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnTru_Click(object sender, EventArgs e)
+        {
+            lbDau.Text = btnTru.Text;
+
+            try
+            {
+                PhanSo ps1 = new PhanSo(int.Parse(txtTuSo1.Text), int.Parse(txtMauSo1.Text));
+                PhanSo ps2 = new PhanSo(int.Parse(txtTuSo2.Text), int.Parse(txtMauSo2.Text));
+                PhanSo kq = ps1.Tru(ps2);
+                txtTuSo3.Text = kq.Tuso.ToString();
+                txtMauSo3.Text = kq.Mauso.ToString();
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnNhan_Click(object sender, EventArgs e)
+        {
+            lbDau.Text = btnNhan.Text;
+
+            try
+            {
+                PhanSo ps1 = new PhanSo(int.Parse(txtTuSo1.Text), int.Parse(txtMauSo1.Text));
+                PhanSo ps2 = new PhanSo(int.Parse(txtTuSo2.Text), int.Parse(txtMauSo2.Text));
+                PhanSo kq = ps1.Nhan(ps2);
+                txtTuSo3.Text = kq.Tuso.ToString();
+                txtMauSo3.Text = kq.Mauso.ToString();
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnChia_Click(object sender, EventArgs e)
+        {
+            lbDau.Text = btnChia.Text;
+
+            try
+            {
+                PhanSo ps1 = new PhanSo(int.Parse(txtTuSo1.Text), int.Parse(txtMauSo1.Text));
+                PhanSo ps2 = new PhanSo(int.Parse(txtTuSo2.Text), int.Parse(txtMauSo2.Text));
+                PhanSo kq = ps1.Chia(ps2);
+                txtTuSo3.Text = kq.Tuso.ToString();
+                txtMauSo3.Text = kq.Mauso.ToString();
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Tieu de: Loi",MessageBoxButtons.OK);
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtTuSo1.Clear();
+            txtMauSo1.Clear();
+            txtTuSo2.Clear();
+            txtMauSo2.Clear();
+            txtTuSo3.Clear();
+            txtMauSo3.Clear();
+        }
+    }
+}
---
+++ BTH3/ClassPhanSo/ClassPhanSo/PhanSo.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPhanSo
+{
+    internal class PhanSo
+    {
+        int tuso, mauso;
+        //Constructor
+        public PhanSo()
+        {
+            this.tuso = 0;
+            this.mauso = 1;
+        }
+        public PhanSo(int tuso, int mauso)
+        {
+            this.tuso = tuso;
+            this.mauso = mauso == 0 ? 1 : mauso;
+        }
+        //Property
+        public int Tuso
+        {
+            get { return tuso;}
+            set { tuso = value; }
+        }
+        public int Mauso
+        {
+            get { return mauso;}
+            set { mauso = value; }
+        }
+        //Method
+        private void Toigian()
+        {
+            int uc = ClassDungChung.UCLN(tuso, mauso);
+            if( uc >0 )
+            {
+                tuso = tuso / uc;
+                mauso = mauso / uc;
+            }
+        }
+        public PhanSo Cong(PhanSo p)
+        {
+            PhanSo kq = new PhanSo();
+            kq.tuso = this.tuso*p.mauso+this.mauso*p.tuso;
+            kq.mauso = this.mauso * p.mauso;
+            kq.Toigian();
+            return kq;
+        }
+        public PhanSo Tru(PhanSo p)
+        {
+            PhanSo kq = new PhanSo();
+            kq.tuso = this.tuso * p.mauso - this.mauso * p.tuso;
+            kq.mauso = this.mauso * p.mauso;
+            kq.Toigian();
+            return kq;
+        }
+        public PhanSo Nhan(PhanSo p)
+        {
+            PhanSo kq = new PhanSo();
+            kq.tuso = this.tuso * p.tuso;
+            kq.mauso = this.mauso * p.mauso;
+            kq.Toigian();
+            return kq;
+        }
+        public PhanSo Chia(PhanSo p)
+        {
+            PhanSo kq = new PhanSo();
+            kq.tuso = this.tuso * p.mauso;
+            kq.mauso = this.mauso * p.tuso;
+            kq.Toigian();
+            return kq;
+        }
+
+    }
+}
