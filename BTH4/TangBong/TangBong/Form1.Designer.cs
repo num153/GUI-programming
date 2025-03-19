181fdc460544305409f361d3c87b799205816e0b
Nam Phuong <157980092+num153@users.noreply.github.com>
2025-03-19 14:41:42 +0700

    chuong trinh tang bong quanh vien

---
+++ BTH4/TangBong/TangBong/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace TangBong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picMeo = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picMeo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picMeo
+            // 
+            this.picMeo.BackgroundImage = global::TangBong.Properties.Resources.cat_meme;
+            this.picMeo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picMeo.Location = new System.Drawing.Point(240, 108);
+            this.picMeo.Name = "picMeo";
+            this.picMeo.Size = new System.Drawing.Size(73, 78);
+            this.picMeo.TabIndex = 0;
+            this.picMeo.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 399);
+            this.Controls.Add(this.picMeo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picMeo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picMeo;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ BTH4/TangBong/TangBong/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TangBong
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int dx = 10, dy = 13;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(picMeo.Left <0 || picMeo.Right > ClientRectangle.Width)
+            {
+                dx = -dx;
+            }
+            if(picMeo.Top < 0 || picMeo.Bottom > ClientRectangle.Height)
+            {
+                dy = -dy;
+            }
+            picMeo.Left += dx;
+            picMeo.Top += dy;
+
+        }
+    }
+}
