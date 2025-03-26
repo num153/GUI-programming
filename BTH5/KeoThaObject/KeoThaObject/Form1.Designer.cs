ef9cbf8eddb0e746f7e5561832f9e4f46cae909a
Nam Phuong <157980092+num153@users.noreply.github.com>
2025-03-26 14:33:33 +0700

    Add files via upload

---
+++ BTH5/KeoThaObject/KeoThaObject/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace KeoThaObject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnBound = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pnBound
+            // 
+            this.pnBound.BackColor = System.Drawing.Color.MistyRose;
+            this.pnBound.Location = new System.Drawing.Point(599, 21);
+            this.pnBound.Name = "pnBound";
+            this.pnBound.Size = new System.Drawing.Size(100, 465);
+            this.pnBound.TabIndex = 1;
+            this.pnBound.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnBound_MouseDown);
+            this.pnBound.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnBound_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(22, 453);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(538, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Dùng phím và chuột để di chuyển ảnh";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 531);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pnBound);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel pnBound;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BTH5/KeoThaObject/KeoThaObject/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace KeoThaObject
+    //SU DUNG FLowLayoutPanel
+{
+    public partial class Form1 : Form
+    {
+        string[] arrFile;
+        Random rand = new Random();
+        int imgSize = 90;
+        int count = 0;
+        Point p;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Dùng phím và chuột để di chuyển ảnh";
+            arrFile = Directory.GetFiles(Application.StartupPath + @"\Images");
+            addNewPicture();
+        }
+        private void addNewPicture()
+        {
+            PictureBox pic = new PictureBox();
+            string file = arrFile[rand.Next(arrFile.Length)];
+            Image img = Image.FromFile(file);
+            pic.Image = img;
+            pic.Location = new Point(0, 0);
+            pic.Width = pic.Height = imgSize;
+            pic.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.Controls.Add(pic);
+            //Su kien chuot
+            pic.MouseDown += Pic_MD;
+            pic.MouseUp += Pic_MU;
+            pic.MouseMove += Pic_MM;
+            //Sap xep anh vao 
+            this.Controls.SetChildIndex(pnBound, this.Controls.Count - 1);
+            //Dat ten cho pic moi khi tao anh
+            count++;
+            pic.Name = count.ToString();
+
+
+        }
+
+        private void Pic_MM(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                PictureBox pic = (PictureBox)sender;
+                int dx = e.X - p.X;
+                int dy = e.Y - p.Y;
+                pic.Left += dx;
+                pic.Top += dy;
+            }
+        }
+
+        private void Pic_MU(object sender, MouseEventArgs e)
+        {
+            PictureBox pic = (PictureBox)sender;
+            if (pnBound.Bounds.Contains(pic.Bounds))
+            {
+                pnBound.Controls.Add(pic);
+                addNewPicture();
+            }
+        }
+
+        private void Pic_MD(object sender, MouseEventArgs e)
+        {
+            p = e.Location;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            Control[] arrCtrl = this.Controls.Find(count.ToString(),false);
+            PictureBox pic = (PictureBox)arrCtrl[0];
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    if (pic.Left > 0) pic.Left -= 5;
+                    break;
+                case Keys.Right :
+                    if (pic.Right < ClientRectangle.Width) pic.Left+=5;
+                    break;
+                case Keys.Down:
+                    if (pic.Bottom < ClientRectangle.Height) pic.Top += 20;
+                    break;
+                case Keys.Up:
+                    if (pic.Top > 0) pic.Top -= 20;
+                    break;
+            }
+            if (pnBound.Bounds.Contains(pic.Bounds))
+            {
+                pnBound.Controls.Add(pic);
+                addNewPicture();
+            }
+        }
+
+        private void pnBound_MouseDown(object sender, MouseEventArgs e)
+        {
+            p = e.Location;
+        }
+
+        private void pnBound_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                int dx = e.X - p.X;
+                int dy = e.Y - p.Y;
+                pnBound.Left += dx;
+                pnBound.Top += dy;
+            }
+        }
+    }
+}
