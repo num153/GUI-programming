ce974936a5f4d8c9dd0a9ae2a8cf2e8164172072
Nam Phuong <157980092+num153@users.noreply.github.com>
2025-03-26 16:45:24 +0700

    Add files via upload

---
+++ BTH5/PhotoAlbum/PhotoAlbum/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace PhotoAlbum
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeFolder = new System.Windows.Forms.TreeView();
+            this.picView = new System.Windows.Forms.PictureBox();
+            this.pnThum = new System.Windows.Forms.FlowLayoutPanel();
+            this.imgList = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // treeFolder
+            // 
+            this.treeFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.treeFolder.ImageIndex = 0;
+            this.treeFolder.ImageList = this.imgList;
+            this.treeFolder.Location = new System.Drawing.Point(12, 21);
+            this.treeFolder.Name = "treeFolder";
+            this.treeFolder.SelectedImageIndex = 0;
+            this.treeFolder.Size = new System.Drawing.Size(289, 471);
+            this.treeFolder.TabIndex = 0;
+            this.treeFolder.AfterCollapse += new System.Windows.Forms.TreeViewEventHandler(this.treeFolder_AfterCollapse);
+            this.treeFolder.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeFolder_BeforeExpand);
+            this.treeFolder.AfterExpand += new System.Windows.Forms.TreeViewEventHandler(this.treeFolder_AfterExpand);
+            this.treeFolder.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeFolder_AfterSelect);
+            // 
+            // picView
+            // 
+            this.picView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picView.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.picView.Location = new System.Drawing.Point(308, 33);
+            this.picView.Name = "picView";
+            this.picView.Size = new System.Drawing.Size(436, 294);
+            this.picView.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picView.TabIndex = 1;
+            this.picView.TabStop = false;
+            // 
+            // pnThum
+            // 
+            this.pnThum.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnThum.AutoScroll = true;
+            this.pnThum.BackColor = System.Drawing.SystemColors.Info;
+            this.pnThum.Location = new System.Drawing.Point(307, 333);
+            this.pnThum.Name = "pnThum";
+            this.pnThum.Size = new System.Drawing.Size(436, 159);
+            this.pnThum.TabIndex = 2;
+            // 
+            // imgList
+            // 
+            this.imgList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgList.ImageStream")));
+            this.imgList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgList.Images.SetKeyName(0, "disk.png");
+            this.imgList.Images.SetKeyName(1, "folder.png");
+            this.imgList.Images.SetKeyName(2, "open.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(770, 516);
+            this.Controls.Add(this.pnThum);
+            this.Controls.Add(this.picView);
+            this.Controls.Add(this.treeFolder);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeFolder;
+        private System.Windows.Forms.PictureBox picView;
+        private System.Windows.Forms.FlowLayoutPanel pnThum;
+        private System.Windows.Forms.ImageList imgList;
+    }
+}
+
---
+++ BTH5/PhotoAlbum/PhotoAlbum/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PhotoAlbum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitTree();
+        }
+        private void InitTree()
+        {
+            //lay ds o dia
+            string[] disk = Directory.GetLogicalDrives();
+            TreeNode node = null;
+            foreach(string d in disk)
+            {
+                node = new TreeNode(d);
+                treeFolder.Nodes.Add(node);
+                node.Nodes.Add("temp");
+            }
+        }
+
+        private void treeFolder_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            TreeNode selectedNote = e.Node;
+            selectedNote.Nodes.Clear();
+            string[] arrDir = Directory.GetDirectories(selectedNote.FullPath);
+            TreeNode node = null;
+            foreach (string dir in arrDir)
+            {
+                node = new TreeNode(Path.GetFileName(dir));
+                node.Nodes.Add("temp");
+                node.ImageIndex = 1;
+                selectedNote.Nodes.Add(node);
+            }
+
+        }
+
+        private void treeFolder_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            try
+            {
+                pnThum.Controls.Clear();
+                TreeNode selectNode = e.Node;//node dang chon
+                string[] arrFile = Directory.GetFiles(selectNode.FullPath);// lay ten
+                foreach (string file in arrFile)
+                {
+                    if (file.ToLower().EndsWith("jpg") ||
+                        file.ToLower().EndsWith("png") ||
+                        file.ToLower().EndsWith("gif") ||
+                        file.ToLower().EndsWith("ico") ||
+                        file.ToLower().EndsWith("bmp"))
+                    {
+                        Image img = Image.FromFile(file);
+                        PictureBox pic = new PictureBox();
+                        pic.Image = img;
+                        pic.SizeMode = PictureBoxSizeMode.StretchImage;
+                        pic.Height = pnThum.Height - 5;
+                        pic.Width = 5 * pic.Height / 4;
+                        pic.Click += Pic_Click;
+                        pic.Cursor = Cursors.Hand;
+                        pnThum.Controls.Add(pic);
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Pic_Click(object sender, EventArgs e)
+        {
+            PictureBox pic = (PictureBox)sender;
+            picView.Image = pic.Image;
+        }
+
+        private void treeFolder_AfterExpand(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Level > 0)
+            {
+                e.Node.ImageIndex = 2;
+                e.Node.SelectedImageIndex = 2;
+            }
+        }
+
+        private void treeFolder_AfterCollapse(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Level > 0)
+            {
+                e.Node.ImageIndex = 1;
+                e.Node.SelectedImageIndex = 1;
+            }
+        }
+    }
+}
