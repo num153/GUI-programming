1870675cafbf6b2cf0f5fd5aa5f241f099140409
Nam Phuong <157980092+num153@users.noreply.github.com>
2025-04-09 09:42:09 +0700

    Add files via upload

---
+++ QuanLiNhanVien/QuanLiNhanVien/FormQuanLy.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace QuanLiNhanVien
+{
+    partial class FormQuanLy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormQuanLy));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateNgaySinh = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdNu = new System.Windows.Forms.RadioButton();
+            this.rdNam = new System.Windows.Forms.RadioButton();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.txtHienThiUsername = new System.Windows.Forms.TextBox();
+            this.imgAnh = new System.Windows.Forms.ImageList(this.components);
+            this.cHoten = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cNgaysinh = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cGioitinh = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lsvNhanVien = new System.Windows.Forms.ListView();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(800, 61);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quản lý nhân viên";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtHoTen.Location = new System.Drawing.Point(129, 67);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(200, 22);
+            this.txtHoTen.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(27, 113);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ngày sinh:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.Location = new System.Drawing.Point(27, 64);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 22);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Họ tên";
+            // 
+            // dateNgaySinh
+            // 
+            this.dateNgaySinh.Location = new System.Drawing.Point(129, 113);
+            this.dateNgaySinh.Name = "dateNgaySinh";
+            this.dateNgaySinh.Size = new System.Drawing.Size(200, 22);
+            this.dateNgaySinh.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdNu);
+            this.groupBox1.Controls.Add(this.rdNam);
+            this.groupBox1.Location = new System.Drawing.Point(31, 163);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(307, 64);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            // 
+            // rdNu
+            // 
+            this.rdNu.AutoSize = true;
+            this.rdNu.Location = new System.Drawing.Point(194, 21);
+            this.rdNu.Name = "rdNu";
+            this.rdNu.Size = new System.Drawing.Size(45, 20);
+            this.rdNu.TabIndex = 0;
+            this.rdNu.TabStop = true;
+            this.rdNu.Text = "Nữ";
+            this.rdNu.UseVisualStyleBackColor = true;
+            // 
+            // rdNam
+            // 
+            this.rdNam.AutoSize = true;
+            this.rdNam.Checked = true;
+            this.rdNam.Location = new System.Drawing.Point(98, 21);
+            this.rdNam.Name = "rdNam";
+            this.rdNam.Size = new System.Drawing.Size(57, 20);
+            this.rdNam.TabIndex = 0;
+            this.rdNam.TabStop = true;
+            this.rdNam.Text = "Nam";
+            this.rdNam.UseVisualStyleBackColor = true;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(63, 266);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(77, 35);
+            this.btnThem.TabIndex = 8;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(161, 266);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(77, 35);
+            this.btnXoa.TabIndex = 8;
+            this.btnXoa.Text = "Xoá";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // txtHienThiUsername
+            // 
+            this.txtHienThiUsername.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtHienThiUsername.Enabled = false;
+            this.txtHienThiUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtHienThiUsername.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.txtHienThiUsername.Location = new System.Drawing.Point(51, 357);
+            this.txtHienThiUsername.Multiline = true;
+            this.txtHienThiUsername.Name = "txtHienThiUsername";
+            this.txtHienThiUsername.Size = new System.Drawing.Size(286, 39);
+            this.txtHienThiUsername.TabIndex = 9;
+            this.txtHienThiUsername.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // imgAnh
+            // 
+            this.imgAnh.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgAnh.ImageStream")));
+            this.imgAnh.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgAnh.Images.SetKeyName(0, "meme.ico");
+            this.imgAnh.Images.SetKeyName(1, "yt.ico");
+            // 
+            // cHoten
+            // 
+            this.cHoten.Text = "Họ tên";
+            // 
+            // cNgaysinh
+            // 
+            this.cNgaysinh.Text = "Ngày sinh";
+            // 
+            // cGioitinh
+            // 
+            this.cGioitinh.Text = "Giới tính";
+            // 
+            // lsvNhanVien
+            // 
+            this.lsvNhanVien.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.cHoten,
+            this.cNgaysinh,
+            this.cGioitinh});
+            this.lsvNhanVien.FullRowSelect = true;
+            this.lsvNhanVien.HideSelection = false;
+            this.lsvNhanVien.Location = new System.Drawing.Point(378, 64);
+            this.lsvNhanVien.Name = "lsvNhanVien";
+            this.lsvNhanVien.Size = new System.Drawing.Size(376, 350);
+            this.lsvNhanVien.SmallImageList = this.imgAnh;
+            this.lsvNhanVien.TabIndex = 10;
+            this.lsvNhanVien.UseCompatibleStateImageBehavior = false;
+            this.lsvNhanVien.View = System.Windows.Forms.View.Details;
+            // 
+            // FormQuanLy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lsvNhanVien);
+            this.Controls.Add(this.txtHienThiUsername);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dateNgaySinh);
+            this.Controls.Add(this.txtHoTen);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "FormQuanLy";
+            this.Text = "Quản lý nhân viên";
+            this.Load += new System.EventHandler(this.FormQuanLy_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateNgaySinh;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdNu;
+        private System.Windows.Forms.RadioButton rdNam;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.TextBox txtHienThiUsername;
+        private System.Windows.Forms.ImageList imgAnh;
+        private System.Windows.Forms.ColumnHeader cHoten;
+        private System.Windows.Forms.ColumnHeader cNgaysinh;
+        private System.Windows.Forms.ColumnHeader cGioitinh;
+        private System.Windows.Forms.ListView lsvNhanVien;
+    }
+}
---
+++ QuanLiNhanVien/QuanLiNhanVien/FormQuanLy.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLiNhanVien
+{
+    public partial class FormQuanLy : Form
+    {
+        public static string username = "";
+        public FormQuanLy()
+        {
+            FormDangNhap f = new FormDangNhap();
+            f.ShowDialog();
+            InitializeComponent();
+        }
+
+        private void FormQuanLy_Load(object sender, EventArgs e)
+        {
+            txtHienThiUsername.Text = username;
+            lsvNhanVien.Columns[0].Width = (int)(lsvNhanVien.Width*0.5);
+            lsvNhanVien.Columns[1].Width = (int)(lsvNhanVien.Width * 0.25);
+            lsvNhanVien.Columns[2].Width = (int)(lsvNhanVien.Width * 0.25);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            ListViewItem item;
+            //can than nho khai bao dung cu phap
+            if (txtHoTen.Text != "")
+            {
+                item = new ListViewItem(txtHoTen.Text); //cot ho ten
+                item.SubItems.Add(dateNgaySinh.Value.ToString("dd/MM/yyyy"));
+                item.SubItems.Add(rdNam.Checked ? "Nam" : "Nữ");
+                item.ImageIndex = rdNam.Checked? 0 : 1;
+                lsvNhanVien.Items.Add(item);
+                txtHoTen.Text = "";
+                txtHoTen.Focus();
+            }
+        }
+        //Chinh full row select de co the chon nguyen dong
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            foreach( ListViewItem i in lsvNhanVien.SelectedItems)
+                {
+                    lsvNhanVien.Items.Remove(i);
+                }
+        }
+    }
+}
