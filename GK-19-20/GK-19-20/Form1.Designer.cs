97fb6b920ed963b682e7a26dad1b8a5a5a1cf8b7
Nam Phuong <157980092+num153@users.noreply.github.com>
2025-04-09 11:43:49 +0700

    Add files via upload

---
+++ GK-19-20/GK-19-20/Form1.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace GK_19_20
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rdLe = new System.Windows.Forms.RadioButton();
+            this.rdChan = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rd1118 = new System.Windows.Forms.RadioButton();
+            this.rd310 = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbTiencon = new System.Windows.Forms.Label();
+            this.lb1 = new System.Windows.Forms.Label();
+            this.lb3 = new System.Windows.Forms.Label();
+            this.lb2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(71, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(108, 106);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(312, 38);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(108, 106);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(594, 38);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(108, 106);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.LightSalmon;
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.rdLe);
+            this.groupBox1.Controls.Add(this.rdChan);
+            this.groupBox1.Location = new System.Drawing.Point(48, 193);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(264, 130);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Chọn chẵn lẻ:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "100",
+            "200",
+            "300",
+            "400",
+            "500"});
+            this.comboBox1.Location = new System.Drawing.Point(127, 89);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(112, 24);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Số tiền cược";
+            // 
+            // rdLe
+            // 
+            this.rdLe.AutoSize = true;
+            this.rdLe.Location = new System.Drawing.Point(140, 50);
+            this.rdLe.Name = "rdLe";
+            this.rdLe.Size = new System.Drawing.Size(43, 20);
+            this.rdLe.TabIndex = 0;
+            this.rdLe.TabStop = true;
+            this.rdLe.Text = "Lẻ";
+            this.rdLe.UseVisualStyleBackColor = true;
+            // 
+            // rdChan
+            // 
+            this.rdChan.AutoSize = true;
+            this.rdChan.Checked = true;
+            this.rdChan.Location = new System.Drawing.Point(31, 50);
+            this.rdChan.Name = "rdChan";
+            this.rdChan.Size = new System.Drawing.Size(59, 20);
+            this.rdChan.TabIndex = 0;
+            this.rdChan.TabStop = true;
+            this.rdChan.Text = "Chẵn";
+            this.rdChan.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.LightSalmon;
+            this.groupBox2.Controls.Add(this.comboBox2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.rd1118);
+            this.groupBox2.Controls.Add(this.rd310);
+            this.groupBox2.Location = new System.Drawing.Point(420, 193);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(264, 130);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Chọn tổng số:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "100",
+            "200",
+            "300",
+            "400",
+            "500"});
+            this.comboBox2.Location = new System.Drawing.Point(127, 89);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(112, 24);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Số tiền cược";
+            // 
+            // rd1118
+            // 
+            this.rd1118.AutoSize = true;
+            this.rd1118.Location = new System.Drawing.Point(140, 50);
+            this.rd1118.Name = "rd1118";
+            this.rd1118.Size = new System.Drawing.Size(60, 20);
+            this.rd1118.TabIndex = 0;
+            this.rd1118.TabStop = true;
+            this.rd1118.Text = "11-18";
+            this.rd1118.UseVisualStyleBackColor = true;
+            // 
+            // rd310
+            // 
+            this.rd310.AutoSize = true;
+            this.rd310.Checked = true;
+            this.rd310.Location = new System.Drawing.Point(31, 50);
+            this.rd310.Name = "rd310";
+            this.rd310.Size = new System.Drawing.Size(53, 20);
+            this.rd310.TabIndex = 0;
+            this.rd310.TabStop = true;
+            this.rd310.Text = "3-10";
+            this.rd310.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(71, 362);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(114, 20);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Giao diện hình";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkGray;
+            this.button1.Location = new System.Drawing.Point(188, 340);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 62);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Quay số";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.Location = new System.Drawing.Point(376, 363);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 29);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Tiền còn lại:";
+            // 
+            // lbTiencon
+            // 
+            this.lbTiencon.BackColor = System.Drawing.Color.Silver;
+            this.lbTiencon.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbTiencon.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbTiencon.Location = new System.Drawing.Point(526, 358);
+            this.lbTiencon.Name = "lbTiencon";
+            this.lbTiencon.Size = new System.Drawing.Size(157, 63);
+            this.lbTiencon.TabIndex = 5;
+            this.lbTiencon.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lb1
+            // 
+            this.lb1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lb1.Location = new System.Drawing.Point(72, 38);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(107, 106);
+            this.lb1.TabIndex = 6;
+            this.lb1.Text = "1";
+            this.lb1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lb3
+            // 
+            this.lb3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lb3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lb3.Location = new System.Drawing.Point(594, 38);
+            this.lb3.Name = "lb3";
+            this.lb3.Size = new System.Drawing.Size(107, 106);
+            this.lb3.TabIndex = 6;
+            this.lb3.Text = "1";
+            this.lb3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lb2
+            // 
+            this.lb2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lb2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lb2.Location = new System.Drawing.Point(313, 38);
+            this.lb2.Name = "lb2";
+            this.lb2.Size = new System.Drawing.Size(107, 106);
+            this.lb2.TabIndex = 6;
+            this.lb2.Text = "1";
+            this.lb2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lb2);
+            this.Controls.Add(this.lb3);
+            this.Controls.Add(this.lb1);
+            this.Controls.Add(this.lbTiencon);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rdLe;
+        private System.Windows.Forms.RadioButton rdChan;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rd1118;
+        private System.Windows.Forms.RadioButton rd310;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbTiencon;
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.Label lb3;
+        private System.Windows.Forms.Label lb2;
+    }
+}
+
---
+++ GK-19-20/GK-19-20/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GK_19_20
+{
+    public partial class Form1 : Form
+    {
+        String imgPath;
+        Random rand = new Random();
+        int dice1=1,dice2=1,dice3 = 1;
+        int tienUser = 1000;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+            imgPath = Application.StartupPath + @"\hinhxucxac\";
+            Init();
+            lbTiencon.Text = tienUser.ToString();
+
+        }
+        private void Init()
+        {
+            pictureBox1.Image = Image.FromFile(imgPath + "1.jpg");
+            pictureBox2.Image = Image.FromFile(imgPath + "3.jpg");
+            pictureBox3.Image = Image.FromFile(imgPath + "1.jpg");
+        }
+        private void HienThi()
+        {
+            if (checkBox1.Checked)
+            {
+                pictureBox1.Image = Image.FromFile(imgPath + dice1.ToString() + ".jpg");
+                pictureBox2.Image = Image.FromFile(imgPath + dice2.ToString() + ".jpg");
+                pictureBox3.Image = Image.FromFile(imgPath + dice3.ToString() + ".jpg");
+                lb1.Visible = false;
+                lb2.Visible = false;
+                lb3.Visible = false;
+            }
+            else
+            {
+                lb1.Text = dice1.ToString();
+                lb2.Text = dice2.ToString();
+                lb3.Text = dice3.ToString();
+                pictureBox1.Image = null;
+                pictureBox2.Image = null;
+                pictureBox3.Image = null;
+                lb1.Visible = true;
+                lb2.Visible = true;
+                lb3.Visible = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Play();
+        }
+        private void Play()
+        {
+            //Xu ly hien thi
+            dice1 = rand.Next(1, 7);
+            dice2 = rand.Next(1, 7);
+            dice3 = rand.Next(1, 7);
+            HienThi();
+
+            //Xu ly tro choi
+            int tongso = dice1 + dice2 + dice3;
+            bool chanle;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            //Hien thi ngay lap tuc khi checkbox thay doi so/hinh
+            HienThi();
+        }
+    }
+}
