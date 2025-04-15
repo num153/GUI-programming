bf4f103fe1fd915ebe4495c231e2935d4b24d54f
Nam Phuong <157980092+num153@users.noreply.github.com>
2025-04-15 15:06:08 +0700

    Add files via upload

---
+++ XuLyChuoi/XuLyChuoi/Form1.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace XuLyChuoi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tieude = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.s1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.s2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.s3 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.vitrichen = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnChen = new System.Windows.Forms.Button();
+            this.btnThaythe = new System.Windows.Forms.Button();
+            this.btnxoa = new System.Windows.Forms.Button();
+            this.btnDao = new System.Windows.Forms.Button();
+            this.btnChuan = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnLay = new System.Windows.Forms.Button();
+            this.tuvitri = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.sokitu = new System.Windows.Forms.TextBox();
+            this.btnKhoiPhuc = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tieude
+            // 
+            this.tieude.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.tieude.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tieude.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.tieude.Location = new System.Drawing.Point(0, 0);
+            this.tieude.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.tieude.Name = "tieude";
+            this.tieude.Size = new System.Drawing.Size(965, 98);
+            this.tieude.TabIndex = 0;
+            this.tieude.Text = "Chương Trình Minh Hoạ Xử Lý Chuỗi";
+            this.tieude.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(105, 115);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Chuỗi 1:";
+            // 
+            // s1
+            // 
+            this.s1.Location = new System.Drawing.Point(195, 115);
+            this.s1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.s1.Name = "s1";
+            this.s1.Size = new System.Drawing.Size(626, 30);
+            this.s1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(105, 173);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Chuỗi 2:";
+            // 
+            // s2
+            // 
+            this.s2.Location = new System.Drawing.Point(195, 173);
+            this.s2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.s2.Name = "s2";
+            this.s2.Size = new System.Drawing.Size(226, 30);
+            this.s2.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(505, 168);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 25);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Chuỗi 3:";
+            // 
+            // s3
+            // 
+            this.s3.Location = new System.Drawing.Point(595, 168);
+            this.s3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.s3.Name = "s3";
+            this.s3.Size = new System.Drawing.Size(226, 30);
+            this.s3.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnChen);
+            this.groupBox1.Controls.Add(this.vitrichen);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(18, 237);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(279, 156);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Chèn chuỗi 2 vào 1";
+            // 
+            // vitrichen
+            // 
+            this.vitrichen.Location = new System.Drawing.Point(28, 70);
+            this.vitrichen.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.vitrichen.Name = "vitrichen";
+            this.vitrichen.Size = new System.Drawing.Size(122, 30);
+            this.vitrichen.TabIndex = 2;
+            this.vitrichen.TextChanged += new System.EventHandler(this.vitrichen_TextChanged);
+            this.vitrichen.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.vitrichen_KeyPress);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(42, 41);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 25);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Vị trí chèn";
+            // 
+            // btnChen
+            // 
+            this.btnChen.Location = new System.Drawing.Point(183, 72);
+            this.btnChen.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnChen.Name = "btnChen";
+            this.btnChen.Size = new System.Drawing.Size(87, 59);
+            this.btnChen.TabIndex = 3;
+            this.btnChen.Text = "Chèn";
+            this.btnChen.UseVisualStyleBackColor = true;
+            this.btnChen.Click += new System.EventHandler(this.btnChen_Click);
+            // 
+            // btnThaythe
+            // 
+            this.btnThaythe.Location = new System.Drawing.Point(401, 268);
+            this.btnThaythe.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnThaythe.Name = "btnThaythe";
+            this.btnThaythe.Size = new System.Drawing.Size(246, 34);
+            this.btnThaythe.TabIndex = 4;
+            this.btnThaythe.Text = "Thay thế s2 bằng s3";
+            this.btnThaythe.UseVisualStyleBackColor = true;
+            this.btnThaythe.Click += new System.EventHandler(this.btnThaythe_Click);
+            // 
+            // btnxoa
+            // 
+            this.btnxoa.Location = new System.Drawing.Point(401, 334);
+            this.btnxoa.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnxoa.Name = "btnxoa";
+            this.btnxoa.Size = new System.Drawing.Size(246, 34);
+            this.btnxoa.TabIndex = 4;
+            this.btnxoa.Text = "Xoá s2 trong s1";
+            this.btnxoa.UseVisualStyleBackColor = true;
+            this.btnxoa.Click += new System.EventHandler(this.btnxoa_Click);
+            // 
+            // btnDao
+            // 
+            this.btnDao.Location = new System.Drawing.Point(678, 268);
+            this.btnDao.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnDao.Name = "btnDao";
+            this.btnDao.Size = new System.Drawing.Size(246, 34);
+            this.btnDao.TabIndex = 4;
+            this.btnDao.Text = "Đảo từ s1";
+            this.btnDao.UseVisualStyleBackColor = true;
+            this.btnDao.Click += new System.EventHandler(this.btnDao_Click);
+            // 
+            // btnChuan
+            // 
+            this.btnChuan.Location = new System.Drawing.Point(678, 334);
+            this.btnChuan.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnChuan.Name = "btnChuan";
+            this.btnChuan.Size = new System.Drawing.Size(246, 34);
+            this.btnChuan.TabIndex = 4;
+            this.btnChuan.Text = "Chuẩn hoá";
+            this.btnChuan.UseVisualStyleBackColor = true;
+            this.btnChuan.Click += new System.EventHandler(this.btnChuan_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnLay);
+            this.groupBox2.Controls.Add(this.sokitu);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.tuvitri);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Location = new System.Drawing.Point(18, 443);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Size = new System.Drawing.Size(522, 156);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Lấy chuỗi con trong s1";
+            // 
+            // btnLay
+            // 
+            this.btnLay.Location = new System.Drawing.Point(426, 58);
+            this.btnLay.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnLay.Name = "btnLay";
+            this.btnLay.Size = new System.Drawing.Size(87, 59);
+            this.btnLay.TabIndex = 3;
+            this.btnLay.Text = "Lấy";
+            this.btnLay.UseVisualStyleBackColor = true;
+            this.btnLay.Click += new System.EventHandler(this.btnLay_Click);
+            // 
+            // tuvitri
+            // 
+            this.tuvitri.Location = new System.Drawing.Point(28, 70);
+            this.tuvitri.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tuvitri.Name = "tuvitri";
+            this.tuvitri.Size = new System.Drawing.Size(122, 30);
+            this.tuvitri.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(42, 41);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 25);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Từ vị trí";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(210, 41);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 25);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Số kí tự";
+            // 
+            // sokitu
+            // 
+            this.sokitu.Location = new System.Drawing.Point(196, 70);
+            this.sokitu.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.sokitu.Name = "sokitu";
+            this.sokitu.Size = new System.Drawing.Size(122, 30);
+            this.sokitu.TabIndex = 2;
+            // 
+            // btnKhoiPhuc
+            // 
+            this.btnKhoiPhuc.Location = new System.Drawing.Point(737, 501);
+            this.btnKhoiPhuc.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnKhoiPhuc.Name = "btnKhoiPhuc";
+            this.btnKhoiPhuc.Size = new System.Drawing.Size(210, 59);
+            this.btnKhoiPhuc.TabIndex = 3;
+            this.btnKhoiPhuc.Text = "Khôi phục";
+            this.btnKhoiPhuc.UseVisualStyleBackColor = true;
+            this.btnKhoiPhuc.Click += new System.EventHandler(this.btnKhoiPhuc_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(965, 612);
+            this.Controls.Add(this.btnKhoiPhuc);
+            this.Controls.Add(this.btnxoa);
+            this.Controls.Add(this.btnChuan);
+            this.Controls.Add(this.btnDao);
+            this.Controls.Add(this.btnThaythe);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.s3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.s2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.s1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tieude);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label tieude;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox s1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox s2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox s3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox vitrichen;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnChen;
+        private System.Windows.Forms.Button btnThaythe;
+        private System.Windows.Forms.Button btnxoa;
+        private System.Windows.Forms.Button btnDao;
+        private System.Windows.Forms.Button btnChuan;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnLay;
+        private System.Windows.Forms.TextBox sokitu;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tuvitri;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnKhoiPhuc;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ XuLyChuoi/XuLyChuoi/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XuLyChuoi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string stin = "Khoa XYZT Nganh Cong nghe thon tin DHM";
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            tieude.Text = tieude.Text.Substring(1)+tieude.Text.Substring(0, 1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tieude.Text = "    CHUONG TRINH MINH HOA XU LY CHUOI     ";
+            s1.Text = stin;
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+        
+        //Bat su kien key preview cua form len de su dung keypress
+        private void vitrichen_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnChen_Click(object sender, EventArgs e)
+        {
+            int pos = int.Parse(vitrichen.Text);
+            if (pos < 0 || pos > s1.Text.Length) return;
+            s1.Text = s1.Text.Insert(pos, s2.Text);
+            vitrichen.Text = "";
+        }
+        //Cai nay hay !
+        private void vitrichen_TextChanged(object sender, EventArgs e)
+        {
+            if(vitrichen.Text == "")
+            {
+                btnChen.Enabled = false;
+            }
+            else
+            {
+                btnChen.Enabled = true;
+            }
+        }
+
+        private void btnThaythe_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(s2.Text)) return;
+            if (s3.Text != "")
+            {
+                s2.Text = s2.Text.Replace(s2.Text, s3.Text);
+            }
+            else
+            {
+                MessageBox.Show("Nhap s3");
+            }
+        }
+
+        private void btnxoa_Click(object sender, EventArgs e)
+        {
+            string string1 = s1.Text.ToLower();
+            string string2 = s2.Text.ToLower();
+            int index=string1.IndexOf(string2);
+
+            while (index>=0)
+            {
+                    string1 = string1.Remove(index, string2.Length);
+                    s1.Text = s1.Text.Remove(index,s2.Text.Length);
+                    index = string1.IndexOf(string2);
+            }
+        }
+
+        private void btnKhoiPhuc_Click(object sender, EventArgs e)
+        {
+            s1.Text = stin;
+        }
+
+        private void btnLay_Click(object sender, EventArgs e)
+        {
+            int pos = int.Parse(tuvitri.Text);
+            if (pos < 0 || pos > s1.Text.Length) return;
+            int numChar = int.Parse(sokitu.Text);
+            s2.Text = s1.Text.Substring(pos, numChar);
+        }
+
+        private void btnDao_Click(object sender, EventArgs e)
+        {
+            char[] token = { ' ', '\t' };
+            string[] arr = s1.Text.Split(token, StringSplitOptions.RemoveEmptyEntries);
+            Array.Reverse(arr);
+            s1.Text = string.Join(" ", arr);
+        }
+
+        private void btnChuan_Click(object sender, EventArgs e)
+        {
+            char[] token = { ' ', '\t' };
+            string[] arr = s1.Text.Split(token, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = arr[i].Substring(0, 1).ToUpper() + arr[i].Substring(1).ToLower();
+            }
+            s1.Text = string.Join(" ", arr);
+        }
+    }
+}
